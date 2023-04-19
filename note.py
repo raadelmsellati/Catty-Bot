b2a6c9e8d48724e5f04305466758df40ad932438
Raad Elmsellati <67578252+raadelmsellati@users.noreply.github.com>
2023-04-19 05:04:41 -0700

    Update note.py

--- note.py
+++ note.py
@@ -10,24 +10,33 @@ def get_random_cat_image_url():
     image_url = data[0]['url']
     return image_url
 
-def send_discord_message(image_url):
+def get_random_cat_fact():
+    url = 'https://catfact.ninja/fact'
+    response = requests.get(url)
+    data = response.json()
+    fact = data['fact']
+    return fact
+
+def send_discord_message(image_url, fact):
     data = {
         'embeds': [
             {
-                'title': 'Isn\'t that cute?',
+                'title': 'Random Cat Picture and Fact',
+                'description': fact,
                 'image': {'url': image_url},
-                'color': 0x1da1f2
             }
         ]
     }
     response = requests.post(DISCORD_WEBHOOK_URL, json=data)
     if response.status_code != 204:
         print(f'Error sending Discord message: {response.text}')
+       
 
 def main():
     while True:
         image_url = get_random_cat_image_url()
-        send_discord_message(image_url)
+        fact = get_random_cat_fact()
+        send_discord_message(image_url, fact)
         time.sleep(300)  
 
 if __name__ == '__main__':
