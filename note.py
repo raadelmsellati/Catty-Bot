e12f5e8a7c34c66a244168819563b79f6332a2bb
Raad Elmsellati <67578252+raadelmsellati@users.noreply.github.com>
2023-04-19 04:10:47 -0700

    Update note.py

--- note.py
+++ note.py
@@ -2,11 +2,15 @@ import time
 from instaloader import Instaloader, Profile
 from discord_webhook import DiscordWebhook
 
+GIN = '365Journey'
+I = '!Journey2023'
+
 INSTAGRAM_USERNAME = 'umasiberian'  # Replace with the desired Instagram username (e.g., 'bbc')
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1098200267733880896/tQIEm4mHDZ6Q_hNnG7A-ujyRkzbzFq0UmUkqFwqvzMiaahHOierBjeaUJz2iPNS-kwCn'  # Replace with your Discord webhook URL
 
 def get_recent_posts(username):
     loader = Instaloader()
+    loader.context.log_in(GIN, I)
     profile = Profile.from_username(loader.context, username)
     posts = list(profile.get_posts())[:5]  # Get the 5 most recent posts
     return posts
